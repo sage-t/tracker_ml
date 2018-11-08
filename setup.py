ffa0ca2809758a031577e72f3293b09099a7dcb8
Sage Thomas <sage.thomas@outlook.com>
2018-11-08 14:01:39 -0700

    setup info

--- setup.py
+++ setup.py
@@ -11,16 +11,23 @@ setuptools.setup(
     description="Machine learning file and parameter version control SDK for www.tracker.ml",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/sage-t/tracker_ml_python_sdk",
+    url="https://github.com/sage-t/tracker_ml",
+    download_url='https://github.com/sage-t/tracker_ml/archive/v0.0.1.tar.gz',
+    keywords=['MACHINE', 'LEARNING', 'VERSION', 'TRACKING', 'TRACKER'],
     packages=setuptools.find_packages(),
     classifiers=[
+        "Development Status :: 3 - Alpha",
         "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.4",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
+        "Intended Audience :: Developers"
     ],
     entry_points={
         'console_scripts': [
             'tracker = tracker_ml.tools.cli:cli'
         ]
-    }
+    },
 )
